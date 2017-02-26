ec8808ec10329b3b917a17bad3f896e37bcdadc5
Brian Kustra <bkustrakb@gmail.com>
2017-02-26 11:55:12 -0600

    Basic server working, very rough

--- server.go
+++ server.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -28,6 +29,25 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
+}
+
+func loadChapter(title string) (*Page, error) {
+	filename := "chapters/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func chapHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadChapter(title)
+	//fmt.Println(
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -76,14 +96,19 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 			http.NotFound(w, r)
 			return
 		}
+		fmt.Println(m[2])
 		fn(w, r, m[2])
 	}
 }
 
 func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/chap/", makeHandler(chapHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
+	fmt.Println("Listening on 8080...")
+	fmt.Println("for example try: http://localhost:8080/view/chapter9")
 	http.ListenAndServe(":8080", nil)
+
 }
